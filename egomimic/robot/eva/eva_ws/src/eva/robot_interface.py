8bb1c54e2b6cb7f19bffc3f27795c6dae9300e5f
ElmoPA <elmoworld2005@gmail.com>
2025-10-27 17:21:40 -0400

    Fix SingleARXInterface bugs and completed it

--- egomimic/robot/eva/eva_ws/src/eva/robot_interface.py
+++ egomimic/robot/eva/eva_ws/src/eva/robot_interface.py
@@ -10,6 +10,9 @@ from abc import ABC, abstractmethod
 from stream_aria import AriaRecorder
 from stream_d405 import RealSenseRecorder
 
+from egomimic.robot.eva.kinematics import EvaKinematicsSolver
+
+
 class Robot_Interface(ABC):
 
     def __init__(self):
@@ -18,7 +21,7 @@ class Robot_Interface(ABC):
             self.cfg = self.__get_config(self.cfg)
         except Exception as e:
             print(f"Failed to load configs.yaml: {e}")
-        
+
         # self.arm = arm
 
         model = self.cfg.get("model", "X5")
@@ -31,14 +34,40 @@ class Robot_Interface(ABC):
         self.robot_config.base_link_name = "base_link"
         self.robot_config.eef_link_name = "link6"
 
-        self.controller_config = (
-            arx5.ControllerConfigFactory.get_instance().get_config(
-                "joint_controller", self.robot_config.joint_dof
-            )
+        self.controller_config = arx5.ControllerConfigFactory.get_instance().get_config(
+            "joint_controller", self.robot_config.joint_dof
         )
 
         self.engaged = True
         self.timestamp = 0
+
+        self.ik = EvaKinematicsSolver(self.robot_urdf)
+
+        camera_cfg = self.cfg.get("cameras")
+        self.cameras = dict()
+        if camera_cfg is None:
+            raise RuntimeError("Camera not configured in config.yaml")
+        if ("cam_high" in camera_cfg) and (
+            camera_cfg["cam_high"].get("enabled", False)
+        ):
+            self.cameras["cam_high"] = AriaRecorder()
+        if ("cam_right_wrist" in camera_cfg) and (
+            camera_cfg["cam_right_wrist"].get("enabled", False)
+        ):
+            self.cameras["cam_right_wrist"] = RealSenseRecorder(
+                camera_cfg["cam_right_wrist"]["serial_number"]
+            )
+        if ("cam_left_wrist" in camera_cfg) and (
+            camera_cfg["cam_left_wrist"].get("enabled", False)
+        ):
+            self.cameras["cam_left_wrist"] = RealSenseRecorder(
+                camera_cfg["cam_left_wrist"]["serial_number"]
+            )
+
+        self.gripper_width = self.cfg.get("gripper_width", None)
+
+        if self.gripper_width is None:
+            raise RuntimeError("Gripper value not initialized in config.yaml")
 
     def __get_config(self, cfg):
         share = get_package_share_directory("eva")
@@ -46,7 +75,7 @@ class Robot_Interface(ABC):
         with open(cfg_path, "r") as f:
             cfg = yaml.safe_load(f) or {}
         return cfg
-    
+
     @abstractmethod
     def _create_controllers(self, cfg):
         pass
@@ -80,14 +109,13 @@ class Robot_Interface(ABC):
         pass
 
 
-
 class SingleARXInterface(Robot_Interface):
     def __init__(self, arm):
-        super.__init__()
-        
-        self.arm = arm        
+        super().__init__()
+
+        self.arm = arm
         self._create_controllers(self.cfg)
-    
+
     def _create_controllers(self, cfg):
         if self.arm == "right":
             interfaces_cfg = cfg.get("interfaces", {})
@@ -98,7 +126,9 @@ class SingleARXInterface(Robot_Interface):
             default_iface = "can1"
             selected_interface = interfaces_cfg.get("left", default_iface)
 
-        self.controller = Arx5JointController(self.robot_config, self.controller_config, selected_interface)
+        self.controller = Arx5JointController(
+            self.robot_config, self.controller_config, selected_interface
+        )
 
         gain = self.controller.get_gain()
 
@@ -109,60 +139,104 @@ class SingleARXInterface(Robot_Interface):
         gain.gripper_kp = 1.0
         gain.gripper_kd = 0.1
 
+        self.ts_offset = 0.2
+
         self.controller.set_gain(gain)
 
-
     def set_joint(self, desired_position):
+        """
+
+        Args:
+            desired_position (np.array): 6 joints + gripper values (0 to 1)
+        """
         if desired_position.shape != (7,):
-            raise ValueError("For Eva, desired position must be of shape (7,) for single arm")
-        
+            raise ValueError(
+                "For Eva, desired position must be of shape (7,) for single arm"
+            )
+
         velocity = np.zeros_like(desired_position) + 0.1
         torque = np.zeros_like(desired_position) + 0.1
 
-        requested= ArxJointState(
+        # you need to set the timestamp this way since timestamp tells controller interpolator what the target it should reach at absolute timestamps
+        cur_joint_state = self.controller.get_joint_state()
+        current_ts = getattr(cur_joint_state, "timestamp", 0.0)
+        self.timestamp = current_ts + self.ts_offset
+
+        requested = ArxJointState(
             desired_position.astype(np.float32),
             velocity.astype(np.float32),
             torque.astype(np.float32),
-            float(self.timestamp)
+            float(self.timestamp),
         )
 
-        requested.gripper_pos = float(self.gripper_cmd) # Elmo had a "- 0.018" here. Check on this
+        requested.gripper_pos = float(
+            desired_position[6] * self.gripper_width
+        )  # TODO Elmo had a "- 0.018" here. Depends on calibration
         requested.gripper_vel = 0.2
         requested.gripper_torque = 0.1
 
         self.controller.set_joint_cmd(requested)
 
-        self.timestamp += 1
-
-
-    def set_pose(self):
-        pass
+    def set_pose(self, pose):
+        if pose.shape != (7,):
+            raise ValueError(
+                "For Eva, target position must be of shape (7,) for single arm"
+            )
+        arm_joints = self.solve_ik(pose[:6])
+        joints = np.concatenate([arm_joints, [pose[6]]])
+        self.set_joint(joints)
 
     def get_obs(self):
         obs = {}
         obs["joint_positions"] = self.get_joint()
         obs["ee_pose"] = self.get_pose()
 
-        #camera logic
-        obs["front_img_1"] = 1
-        
+        # camera logic
+        obs["front_img_1"] = self.cameras["cam_high"].get_image()
+
         if self.arm == "right":
-            obs["right_wrist_img"] = 1
+            obs["right_wrist_img"] = self.cameras["cam_right_wrist"].get_image()
         elif self.arm == "left":
-            obs["left_wrist_img"] = 1
+            obs["left_wrist_img"] = self.cameras["cam_left_wrist"].get_image()
 
         return obs
 
-    @staticmethod
-    def solve_ik():
-        pass
+    # removed static since can't figure out how to create ik when robot_urdf is not static
+    def solve_ik(self, ee_pose):
+        if ee_pose.shape != (6,):
+            raise ValueError(
+                "For Eva, target position must be of shape (7,) for single arm"
+            )
+        pos_xyz = ee_pose[:3]
+        quat_xyzw = R.from_euler(
+            "xyz", ee_pose[3:6], degrees=False
+        ).as_quat()  # scipy output xyzw
+        arm_joints = self.ik.inverse_kinematics(pos_xyz, quat_xyzw, self.get_joint())
+        return arm_joints
 
     def get_joint(self):
-        curr_joints = self.controller.get_joint_state()
-        return curr_joints
+        joints = self.controller.get_joint_state()
+        arm_joints = joints.pos()
+        gripper = getattr(joints, "gripper_pos", 0.0)
+        joints = np.array(
+            [
+                arm_joints[0],
+                arm_joints[1],
+                arm_joints[2],
+                arm_joints[3],
+                arm_joints[4],
+                arm_joints[5],
+                gripper,
+            ]
+        )
+        return joints
 
-    #Add docstrings. EEpose has a lot of conventions.
     def get_pose(self):
+        """
+
+        Returns:
+            x,y,z,r,p,y
+        """
         joints = self.get_joint()
 
         chain = pk.build_serial_chain_from_urdf(open(self.robot_urdf).read(), "link6")
@@ -170,44 +244,47 @@ class SingleARXInterface(Robot_Interface):
         x, y, z = matrix[:, :3, 3]
 
         quat = pk.matrix_to_quaternion(matrix[:, :3, :3])
-        R = R.from_quat(quat)
-        r, p, yaw = R.as_euler('xyz', degrees=True)
-        
-        return [x, y, z, r, p, yaw]
+        rot = R.from_quat(quat)
+        r, p, y = rot.as_euler("xyz", degrees=True)
 
+        return [x, y, z, r, p, y]
 
     def set_home(self):
         self.controller.reset_to_home()
 
 
-
-
 class DualARXInterface(Robot_Interface):
     def __init__(self, arm):
         super.__init__()
-        
+
         self.arm = arm
         self.__create_controllers(self.cfg)
-    
+
     def _create_controllers(self, cfg):
         if self.arm == "both":
             interfaces_cfg = cfg.get("interfaces", {})
-            
+
             default_right_iface = "can2"
             selected_right_interface = interfaces_cfg.get("right", default_right_iface)
 
             default_left_iface = "can1"
             selected_left_interface = interfaces_cfg.get("left", default_left_iface)
 
-            self.right_controller = Arx5JointController(self.robot_config, self.controller_config, selected_right_interface)
-            self.left_controller = Arx5JointController(self.robot_config, self.controller_config, selected_left_interface)
+            self.right_controller = Arx5JointController(
+                self.robot_config, self.controller_config, selected_right_interface
+            )
+            self.left_controller = Arx5JointController(
+                self.robot_config, self.controller_config, selected_left_interface
+            )
 
             right_gain = self.right_controller.get_gain()
             left_gain = self.left_controller.get_gain()
 
-            kp = np.array([6.225, 17.225, 18.225, 12.225, 8.225, 6.225], dtype=np.float64)
+            kp = np.array(
+                [6.225, 17.225, 18.225, 12.225, 8.225, 6.225], dtype=np.float64
+            )
             kd = np.array([2.0, 2.0, 2.0, 2.0, 2.0, 2.0], dtype=np.float64)
-            
+
             right_gain.kp()[:] = kp
             right_gain.kd()[:] = kd
             right_gain.gripper_kp = 1.0
@@ -225,11 +302,15 @@ class DualARXInterface(Robot_Interface):
             default_iface = "can2"
             selected_interface = interfaces_cfg.get("right", default_iface)
 
-            self.right_controller = Arx5JointController(self.robot_config, self.controller_config, selected_interface)
+            self.right_controller = Arx5JointController(
+                self.robot_config, self.controller_config, selected_interface
+            )
 
             gain = self.right_controller.get_gain()
 
-            kp = np.array([6.225, 17.225, 18.225, 12.225, 8.225, 6.225], dtype=np.float64)
+            kp = np.array(
+                [6.225, 17.225, 18.225, 12.225, 8.225, 6.225], dtype=np.float64
+            )
             kd = np.array([2.0, 2.0, 2.0, 2.0, 2.0, 2.0], dtype=np.float64)
             gain.kp()[:] = kp
             gain.kd()[:] = kd
@@ -242,11 +323,15 @@ class DualARXInterface(Robot_Interface):
             default_iface = "can1"
             selected_interface = interfaces_cfg.get("left", default_iface)
 
-            self.right_controller = Arx5JointController(self.robot_config, self.controller_config, selected_interface)
+            self.right_controller = Arx5JointController(
+                self.robot_config, self.controller_config, selected_interface
+            )
 
             gain = self.right_controller.get_gain()
 
-            kp = np.array([6.225, 17.225, 18.225, 12.225, 8.225, 6.225], dtype=np.float64)
+            kp = np.array(
+                [6.225, 17.225, 18.225, 12.225, 8.225, 6.225], dtype=np.float64
+            )
             kd = np.array([2.0, 2.0, 2.0, 2.0, 2.0, 2.0], dtype=np.float64)
             gain.kp()[:] = kp
             gain.kd()[:] = kd
@@ -255,24 +340,31 @@ class DualARXInterface(Robot_Interface):
 
             self.right_controller.set_gain(gain)
 
-
     def set_joint(self, desired_position):
-        if (self.arm == "right" or self.arm == "left") and desired_position.shape != (7,):
-            raise ValueError("For Eva, desired position must be of shape (7,) for arm: " + self.arm)
+        if (self.arm == "right" or self.arm == "left") and desired_position.shape != (
+            7,
+        ):
+            raise ValueError(
+                "For Eva, desired position must be of shape (7,) for arm: " + self.arm
+            )
         elif self.arm == "both" and desired_position.shape != (14,):
-            raise ValueError("For Eva, desired position must be of shape (14,) for arm: " + self.arm)
-        
+            raise ValueError(
+                "For Eva, desired position must be of shape (14,) for arm: " + self.arm
+            )
+
         velocity = np.zeros_like(desired_position) + 0.1
         torque = np.zeros_like(desired_position) + 0.1
 
-        requested= ArxJointState(
+        requested = ArxJointState(
             desired_position.astype(np.float32),
             velocity.astype(np.float32),
             torque.astype(np.float32),
-            float(self.timestamp)
+            float(self.timestamp),
         )
 
-        requested.gripper_pos = float(self.gripper_cmd) # Elmo had a "- 0.018" here. Check on this
+        requested.gripper_pos = float(
+            self.gripper_cmd
+        )  # Elmo had a "- 0.018" here. Check on this
         requested.gripper_vel = 0.2
         requested.gripper_torque = 0.1
 
@@ -286,7 +378,6 @@ class DualARXInterface(Robot_Interface):
 
         self.timestamp += 1
 
-
     def set_pose(self):
         pass
 
@@ -295,16 +386,13 @@ class DualARXInterface(Robot_Interface):
         obs["joint_positions"] = self.get_joint()
         obs["ee_pose"] = self.get_pose()
 
-        #camera logic
-        obs["front_img_1"] = 1
-        
+        # camera logic
+        obs["front_img_1"] = self.cameras["cam_high"].get_image()
+
         if self.arm == "right":
-            obs["right_wrist_img"] = 1
+            obs["right_wrist_img"] = self.cameras["right_wrist_img"].get_image()
         elif self.arm == "left":
-            obs["left_wrist_img"] = 1
-        elif self.arm == "both":
-            obs["right_wrist_img"] = 1
-            obs["left_wrist_img"] = 1
+            obs["left_wrist_img"] = self.cameras["left_wrist_img"].get_image()
 
         return obs
 
@@ -323,7 +411,7 @@ class DualARXInterface(Robot_Interface):
             curr_joints = np.concatenate((curr_left_joints, curr_right_joints), axis=0)
         return curr_joints
 
-    #Add docstrings. EEpose has a lot of conventions.
+    # Add docstrings. EEpose has a lot of conventions.
     def get_pose(self):
         joints = self.get_joint()
         chain = pk.build_serial_chain_from_urdf(open(self.robot_urdf).read(), "link6")
@@ -331,9 +419,8 @@ class DualARXInterface(Robot_Interface):
         x, y, z = matrix[:, :3, 3]
         quat = pk.matrix_to_quaternion(matrix[:, :3, :3])
         R = R.from_quat(quat)
-        r, p, yaw = R.as_euler('xyz', degrees=True)
+        r, p, yaw = R.as_euler("xyz", degrees=True)
         return [x, y, z, r, p, yaw]
-
 
     def set_home(self):
         if self.arm == "right":
