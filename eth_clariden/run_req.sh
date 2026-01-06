4b250a74d27887081818f68aac2d0fc0b98ebb83
JIAQI CHEN <jiaqchen@ethz.ch>
2026-01-06 18:49:48 +0100

    adding multinode support and running configs as env variables

--- eth_clariden/run_req.sh
+++ eth_clariden/run_req.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
-#SBATCH --job-name=ego_ee_REQ
+#SBATCH --job-name=fold_clothes
 #SBATCH --account=a144
-#SBATCH --output=slurm-ego_ee_REQ-%j.out
-#SBATCH --error=slurm-ego_ee_REQ-%j.err
-#SBATCH --nodes=1
+#SBATCH --output=/iopsstor/scratch/cscs/jiaqchen/egomim_out/multi_node_slurm_out/50hz/fold_clothes/slurm-fold_clothes-%j-%t.out
+#SBATCH --error=/iopsstor/scratch/cscs/jiaqchen/egomim_out/multi_node_slurm_out/50hz/fold_clothes/slurm-fold_clothes-%j-%t.err
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=4
-#SBATCH --gpus=4
-#SBATCH --time=12:00:00
+#SBATCH --gpus-per-node=4
+#SBATCH --time=00:30:00
 #SBATCH --partition=normal
 #SBATCH --environment=/users/jiaqchen/.edf/faive2lerobot.toml
 #SBATCH --requeue
-#SBATCH --signal=USR1@600
+#SBATCH --signal=SIGUSR1@600
+
+# Stop the script if a command fails or if an undefined variable is used
+set -eo pipefail
+
+# The sbatch script is executed by only one node.
+echo "[sbatch-master] running on $(hostname)"
+
+echo "[sbatch-master] SLURM_NODELIST: $SLURM_NODELIST"
+echo "[sbatch-master] SLURM_NNODES: $SLURM_NNODES"
+echo "[sbatch-master] SLURM_NODEID: $SLURM_NODEID"
+
+echo "[sbatch-master] define some env vars that will be passed to the compute nodes"
+
+# The defined environment vars will be shared with the other compute nodes.
+export MASTER_ADDR=$(scontrol show hostname "$SLURM_NODELIST" | head -n1)  
+export MASTER_PORT=12345   # Choose an unused port
+export WORLD_SIZE=$(( SLURM_NNODES * SLURM_NTASKS_PER_NODE ))
 
 
 # Print job information
@@ -26,43 +43,30 @@ nvidia-smi --query-gpu=memory.total --format=csv
 # Lightning will automatically requeue the job if it crashes, so following is unnecessary: On SIGUSR1, request requeue and exit gracefully
 # trap 'echo "$(date -Ins) SIGUSR1 received; requeuing..."; scontrol requeue "$SLURM_JOB_ID"; exit 0' USR1
 
-# Check if ckpt_path exists, if not then ckpt_path=null, otherwise use the path
-export frame_type=ee_frame
-export hydra_run_dir=/iopsstor/scratch/cscs/jiaqchen/egomim_out/requeue/cup_${frame_type}
+export task=fold_clothes
+export frame_type=base_frame
+export domain=eve_bimanual
+export hydra_run_dir=/iopsstor/scratch/cscs/jiaqchen/egomim_out/multi_node/50hz/${task}/${task}_${frame_type}
 export ckpt_path=${hydra_run_dir}/checkpoints/last.ckpt
 ckpt_path=$( [[ -f "$ckpt_path" ]] && echo "$ckpt_path" || echo null )
 echo "CHECKPOINT PATH! ckpt_path: $ckpt_path"
 
-export dataset_root=/iopsstor/scratch/cscs/jiaqchen/data/EGOMIM/srl_data/output/release/cup_lerobot_${frame_type}
+export dataset_root=/iopsstor/scratch/cscs/jiaqchen/data/EGOMIM/srl_data/output/release_2_0/${task}_lerobot_${frame_type}
 
-# Use srun to launch all 4 processes simultaneously for DDP
-# Similar to the example script, use bash -c to ensure venv is activated in each process
-# SLURM allocates 4 tasks (--ntasks-per-node=4), and srun launches one process per task
-# Each process will source clariden.sh to activate venv and set up environment variables
-# Expose following variables to sbatch:
-# - name
-# - description
-# - chosen_frame
-# - ckpt_path
-# - hydra.run.dir
-# - data.train_datasets.dataset1.root
-# - data.valid_datasets.dataset1.root
 CMD="
 source /capstor/store/cscs/swissai/a144/jiaqchen/egoverse/EgoVerse/eth_clariden/clariden.sh
 cd /iopsstor/scratch/cscs/jiaqchen/egomim_out
-python /capstor/store/cscs/swissai/a144/jiaqchen/egoverse/EgoVerse/egomimic/trainHydra.py \
-    name=cup_${frame_type} \
-    description=cup_${frame_type} \
+exec python /capstor/store/cscs/swissai/a144/jiaqchen/egoverse/EgoVerse/egomimic/trainHydra.py \
+    name=${task}_${frame_type} \
+    description=${task}_${frame_type} \
     chosen_frame=${frame_type} \
     ckpt_path=$ckpt_path \
     hydra.run.dir=$hydra_run_dir \
+    trainer.num_nodes=$SLURM_NNODES \
     data.train_datasets.dataset1.root=$dataset_root \
-    data.valid_datasets.dataset1.root=$dataset_root $@
+    data.valid_datasets.dataset1.root=$dataset_root \
+    model.robomimic_model.domains=[$domain] $@
 "
-srun bash -c "$CMD"
+srun --signal=SIGUSR1@600 bash -c "$CMD"
 
-# Copy the results to capstor
-
-
-# Print completion information
 echo "Job finished at: $(date)"
