6ee76bef327fa6a525e3c16375a00b4a9ac4f809
Aidan Gao <agao81@gatech.edu>
2026-02-03 15:56:29 -0500

    checkpoint commit to sync w ryan new zarr episode/dataset

--- egomimic/rldb/zarr/__init__.py
+++ egomimic/rldb/zarr/__init__.py
@@ -3,15 +3,23 @@ Zarr-based dataset implementations for EgoVerse.
 """
 
 from egomimic.rldb.zarr.zarr_dataset import (
+    DummyZarrEpisode,
+    EpisodeResolver,
     ZarrDataset,
     ZarrDatasetMetadata,
     ZarrDatasetView,
     RLDBZarrDataset,
+    MultiRLDBZarrDataset,
+    S3RLDBZarrDataset,
 )
 
 __all__ = [
+    "DummyZarrEpisode",
+    "EpisodeResolver",
     "ZarrDataset",
     "ZarrDatasetMetadata",
     "ZarrDatasetView",
     "RLDBZarrDataset",
+    "MultiRLDBZarrDataset",
+    "S3RLDBZarrDataset",
 ]
