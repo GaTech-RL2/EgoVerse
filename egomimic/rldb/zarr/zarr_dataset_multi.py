ae603a0f3de3b8c8d91127ad89a87de760bd51dc
Aidan <aidangao345@gmail.com>
2026-05-14 18:39:54 -0400

    fixed datset nonexist bug

--- egomimic/rldb/zarr/zarr_dataset_multi.py
+++ egomimic/rldb/zarr/zarr_dataset_multi.py
@@ -804,16 +804,11 @@ class LocalEpisodeResolver(EpisodeResolver):
                 raise ValueError(
                     "No valid episodes found for allowed_episode_ids in the local directory."
                 )
-        else:
-            filtered_paths = self._get_local_filtered_paths(
-                self.folder_path, filters, debug=self.debug
-            )
+            return datasets
 
-        if self.allowed_episode_ids is not None:
-            filtered_paths = [
-                (p, h) for p, h in filtered_paths if h in self.allowed_episode_ids
-            ]
-
+        filtered_paths = self._get_local_filtered_paths(
+            self.folder_path, filters, debug=self.debug
+        )
         valid_folder_names = {folder_name for _, folder_name in filtered_paths}
         logger.info(f"Valid folder names: {valid_folder_names}")
         if not valid_folder_names:
@@ -821,11 +816,9 @@ class LocalEpisodeResolver(EpisodeResolver):
                 "No valid collection names from local filtering: "
                 "filters matched no episodes in the local directory."
             )
-
-            datasets = self._load_zarr_datasets(
-                search_path=self.folder_path, valid_folder_names=valid_folder_names
-            )
-
+        datasets = self._load_zarr_datasets(
+            search_path=self.folder_path, valid_folder_names=valid_folder_names
+        )
         return datasets
 
 
